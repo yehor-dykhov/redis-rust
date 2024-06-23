c1b9142ad5a75146631fd6c318d349c91d7c0e76
Yehor Dykhov <hyoustone@yahoo.com>
2024-06-23 21:38:30 +0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::io::{Read, Write};
 use std::net::TcpListener;
+use std::str;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                let buf = &mut [0; 14];
+                let _ = &_stream.read(buf).unwrap();
+                let request_txt = str::from_utf8(buf).unwrap();
+
+                if request_txt == "*1\r\n$4\r\nPING\r\n" {
+                    _stream.write_all(b"+PONG\r\n").unwrap()
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
